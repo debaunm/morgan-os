c885ab855821b514031d22bfe4385601fd9859a3
debaunm <m@morgandebaun.com>
2026-02-08 10:41:57 -0600

    Initial project setup: Vite + React PWA config

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MorganOS from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MorganOS />
+  </React.StrictMode>,
+)
+
+// Register service worker for PWA
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').catch(() => {});
+  });
+}
